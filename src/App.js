65f1eb96cb30261c02ea6982159ad724187a4cb6
Jason Neale <jason@novus.one>
2020-02-26 22:50:39 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+// Package imports
+import React from 'react'
+
+/**
+ * ### Routes - 
+ * 1. Login
+ * 2. Register
+ * 3. Dashboard
+ * 4. Activities
+ * 5. Post
+ * 6. View
+ * 7. Protected Application Route
+ */
 
 function App() {
   return (
     <div className="App">
-
+        <h1>Hello I'm the App!</h1>
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
