c903266f9c8dc10854c0fa141846f6af6cc7330a
Jason Neale <jason@novus.one>
2020-02-27 20:28:17 -0500

    Updated routes

--- src/App.js
+++ src/App.js
@@ -29,11 +29,21 @@ function App() {
                 <div className="col form-wrapper">
                     <div className="row">
                         <div className="col-12">
-                            <nav className="comp-nav">
-                                <Link to="/login">Login</Link>
-                                <Link to="/register">Register</Link>
-                            </nav>
-                            <hr />
+                            <div className="row">
+                                <div className="col-12">
+                                    <img className="form-logo" alt="co-make logo" src="images/co-make-logo.png" />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-12">
+                                    <nav className="comp-nav">
+                                        <Link exact to="/">Home</Link>
+                                        <Link to="/login">Login</Link>
+                                        <Link to="/register">Register</Link>
+                                    </nav>
+                                    <hr />
+                                </div>
+                            </div>
                             <Route path="/login" render={renderProps => {
                                 return <LoginForm />
                             }} />
