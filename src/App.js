258a92356aedb197dae3e3efff146840ac7a7d0e
Jason Neale <jason@novus.one>
2020-03-01 21:00:31 -0500

    Fixed component rendering issues

--- src/App.js
+++ src/App.js
@@ -1,64 +1,35 @@
 // Package imports
 import React from 'react'
-import { Route, Link, Redirect } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 // Component Imports
-import LoginForm from './components/LoginForm'
-import RegisterForm from './components/RegisterForm'
 import Dashboard from './components/Dashboard'
+import Home from './components/Home'
 
-/**
- * ### Routes - 
- * 1. Login
- * 2. Register
- * 3. Dashboard
- * 4. Activities
- * 5. Post
- * 6. View
- * 7. Protected Application Route
- */
 
 function App() {
-    return (
-        <div className="row App">
-            <Route path="/dashboard"><Dashboard /></Route>
-            <Route exact path="/dashboard/:userid/activities" />
-            <Route exact path="/post/:userid" />
-            <Route exact path="/view/:postid" />
-            <Redirect from="/" to="login" />
+    const location = useLocation()
 
-            <div className="row page-wrapper">
-                <div className="col form-wrapper">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="row">
-                                <div className="col-12">
-                                    <img className="form-logo" alt="co-make logo" src="/images/co-make-logo.png" />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-12">
-                                    <nav className="comp-nav">
-                                        <Link to="/">Home</Link>
-                                        <Link to="/login">Login</Link>
-                                        <Link to="/register">Register</Link>
-                                        <Link to="/dashboard">Dashboard</Link>
-                                    </nav>
-                                    <hr />
-                                </div>
-                            </div>
-                            <Route path="/login" render={renderProps => {
-                                return <LoginForm {...renderProps} />
-                            }} />
-                            <Route path="/register" render={renderProps => {
-                                return <RegisterForm {...renderProps} />
-                            }} />
-                        </div>
-                    </div>
+    console.log('Location', location)
+
+    if (location.pathname === "/") {
+        return (
+            <div className="row App">
+                <Route path="/dashboard" component={Dashboard} />
+
+                <div className="row page-wrapper">
+                    <Home />
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div>
+                <Route path="/dashboard" component={Dashboard} />
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+// Package imports
+import React from 'react'
+import { Route, Link } from 'react-router-dom'
+
+// Component Imports
+import LoginForm from './LoginForm'
+import RegisterForm from './RegisterForm'
+
+
+function Home() {
+    return (
+        <div className="col form-wrapper">
+            <div className="row">
+                <div className="col-12">
+                    <div className="row">
+                        <div className="col-12">
+                            <img className="form-logo" alt="co-make logo" src="/images/co-make-logo.png" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12">
+                            <nav className="comp-nav">
+                                <Link to="/">Home</Link>
+                                <Link to="/login">Login</Link>
+                                <Link to="/register">Register</Link>
+                                <Link to="/dashboard">Dashboard</Link>
+                            </nav>
+                            <hr />
+                        </div>
+                    </div>
+                    <Route path="/login" render={renderProps => {
+                        return <LoginForm {...renderProps} />
+                    }} />
+                    <Route path="/register" render={renderProps => {
+                        return <RegisterForm {...renderProps} />
+                    }} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
