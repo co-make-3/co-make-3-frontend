160546e6a5f51dbf936c9aab1bbc5be8edaa9d8c
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-03-02 23:01:28 -0500

    get post data

--- src/components/ViewPosts.js
+++ src/components/ViewPosts.js
@@ -1,7 +1,18 @@
 // Package imports
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { axiosWithAuth } from '../axiosWithAuth/axiosWithAuth'
 
 function ViewPosts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth().get('http://co-make-3.herokuapp.com/api/posts')
+            .then(res => {
+                console.log(res.data)
+                setPosts(res.data)
+            })
+    }, [])
+
     return (
         <div className="row">
             <div className="col-12">
