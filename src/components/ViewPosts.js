7e315a6f24346e586d53ecf9c46d907bc6f53258
Jason Neale <jason@novus.one>
2020-03-03 23:20:30 -0500

    Updated

--- src/components/ViewPosts.js
+++ src/components/ViewPosts.js
@@ -1,6 +1,7 @@
 // Package imports
 import React, { useState, useEffect } from 'react'
 import { axiosWithAuth } from '../axiosWithAuth/axiosWithAuth'
+import { Link } from 'react-router-dom'
 
 import Post from './Post';
 
@@ -22,44 +23,12 @@ function ViewPosts() {
 
                 <div className="row page-title">
                     <div className="col-12 text-left">
-                        <h1>View Posts</h1>
-                        {posts.map(post => <Post post={post}/>)}     
+                        <h1>View Posts</h1>   
                     </div>
                 </div>
 
-                <div className="row content-wrapper">
-                    <div className="col-12">
-                        <table class="table table-striped table-dark">
-                            <thead>
-                                <tr>
-                                    <th className="table-h-c-1" scope="col">ID#</th>
-                                    <th className="table-h-c-2" scope="col">Description</th>
-                                    <th className="table-h-c-3" scope="col">Up Votes</th>
-                                    <th className="table-h-c-4" scope="col">Down Votes</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>,jhcvnhvhmnjv,hmvb....</td>
-                                    <td>4857</td>
-                                    <td>24</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>,mjb;.,mjb.,jb.,mj....</td>
-                                    <td>1540</td>
-                                    <td>3</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>j,hvgljhvgbljhb....</td>
-                                    <td>754</td>
-                                    <td>1</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                <div className="row post-wrapper">
+                    {posts.map(post => <Post post={post}/>)}  
                 </div>
 
             </div>
