755f4a7aaa22bb8ee84cdf7da6f745e8ebaa3bb3
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2020-03-05 21:53:03 -0500

    upvote system

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 // Package Imports
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { axiosWithAuth } from '../axiosWithAuth/axiosWithAuth'
 
@@ -10,7 +10,20 @@ function Post({ post }) {
     if (post.post_image_url === "") {
         post.post_image_url = "https://pngimage.net/wp-content/uploads/2018/05/default-png-6.png"
     }
-    
+
+    const [votes, setVotes] = useState(post.votes)
+
+    function handleUpvote() {
+        axiosWithAuth().post(`http://co-make-3.herokuapp.com/api/posts/${postID}/increment/votes`)
+            .then(res => {
+                axiosWithAuth().get(`http://co-make-3.herokuapp.com/api/posts/${postID}`)
+                    .then(res => {
+                        console.log(res)
+                        setVotes(res.data.votes);
+                    })
+            })
+    }
+
     return(
         <div className='card post'>
             <img className="card-img-top" src={post.post_image_url} alt={'post image'} />
@@ -22,11 +35,11 @@ function Post({ post }) {
                     <div className="card-details">
                         <p><strong>City: </strong>{post.city}</p>
                         <p><strong>Zip Code: </strong>{post.zip_code}</p>
-                        <p><strong>Votes: </strong>{post.votes}</p>
+                        <p><strong>Votes: </strong>{votes}</p>
                     </div>
                 </div>
             </div>
-            <button className="btn btn-primary btn-upvote">Upvote</button>
+            <button onClick={() => handleUpvote()} className="btn btn-primary btn-upvote">Upvote</button>
         </div>
     )
 }
